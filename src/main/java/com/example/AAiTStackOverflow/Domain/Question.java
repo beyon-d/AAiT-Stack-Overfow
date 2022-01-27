9b88eeca1292fc9142ea0fd062d0d4526223f461
birehan <91595957+birehan@users.noreply.github.com>
2022-01-27 11:43:51 -0800

    Update Question.java

--- src/main/java/com/example/AAiTStackOverflow/Domain/Question.java
+++ src/main/java/com/example/AAiTStackOverflow/Domain/Question.java
@@ -1,1 +1,45 @@
+package com.example.AAiTStackOverflow.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    @Size(message = "Title can not be less than 5 characters.", min = 5)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "text", length = 65536, nullable = false)
+    @Size(message = "Description can not be less than 20 characters.", min = 20)
+    @NotBlank(message = "Question body can't be empty")
+    private String description;
+
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "author_id")
+
+    private Account author;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
+
+}
