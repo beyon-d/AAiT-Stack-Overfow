4ce765be01ec9720a8cd3398ef14c6e7b5e8e22a
birehan <91595957+birehan@users.noreply.github.com>
2022-01-27 11:52:08 -0800

    Update CustomUserDetailsService.java

--- src/main/java/com/example/AAiTStackOverflow/CustomUserDetailsService.java
+++ src/main/java/com/example/AAiTStackOverflow/CustomUserDetailsService.java
@@ -1,1 +1,30 @@
+package com.example.AAiTStackOverflow;
+
+import com.example.AAiTStackOverflow.Domain.Account;
+import com.example.AAiTStackOverflow.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
 
